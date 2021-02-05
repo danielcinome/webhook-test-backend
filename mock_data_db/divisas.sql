e580f38afd94a9f955fd896565677549b275d430
danielcinome <danielchinome987@gmail.com>
2021-02-05 12:19:14 -0500

    mockup data database

---
+++ mock_data_db/divisas.sql
@@ -0,0 +1,26 @@
+create table divisas (
+	id INT,
+	name VARCHAR(50),
+	exchange_pair VARCHAR(50),
+	created_at DATE
+);
+insert into divisas (id, name, exchange_pair, date) values (1, 'EUR-USD', '1.2180', '1/1/2021');
+insert into divisas (id, name, exchange_pair, date) values (2, 'EUR-USD', '1.2252', '1/5/2021');
+insert into divisas (id, name, exchange_pair, date) values (3, 'EUR-USD', '1.2186', '1/11/2021');
+insert into divisas (id, name, exchange_pair, date) values (4, 'EUR-USD', '1.2151', '1/15/2021');
+insert into divisas (id, name, exchange_pair, date) values (5, 'EUR-USD', '1.2139', '1/20/2021');
+insert into divisas (id, name, exchange_pair, date) values (6, 'EUR-USD', '1.2169', '1/25/2021');
+
+insert into divisas (id, name, exchange_pair, date) values (11, 'CLP-USD', '0,001406', '1/3/2021');
+insert into divisas (id, name, exchange_pair, date) values (12, 'CLP-USD', '0,001404', '1/8/2021');
+insert into divisas (id, name, exchange_pair, date) values (13, 'CLP-USD', '0,001355', '1/13/2021');
+insert into divisas (id, name, exchange_pair, date) values (14, 'CLP-USD', '0,001361', '1/18/2021');
+insert into divisas (id, name, exchange_pair, date) values (15, 'CLP-USD', '0,001374', '1/23/2021');
+
+insert into divisas (id, name, exchange_pair, date) values (21, 'PEN-USD', '0.2763', '1/2/2021');
+insert into divisas (id, name, exchange_pair, date) values (22, 'PEN-USD', '0.2757', '1/5/2021');
+insert into divisas (id, name, exchange_pair, date) values (23, 'PEN-USD', '0.2806', '1/11/2021');
+insert into divisas (id, name, exchange_pair, date) values (24, 'PEN-USD', '0.2771', '1/15/2021');
+insert into divisas (id, name, exchange_pair, date) values (25, 'PEN-USD', '0.2768', '1/20/2021');
+insert into divisas (id, name, exchange_pair, date) values (26, 'PEN-USD', '0.2753', '1/25/2021');
+insert into divisas (id, name, exchange_pair, date) values (27, 'PEN-USD', '0.2750', '2/1/2021');
